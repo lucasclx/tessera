77b597453cc2bc234cf7eca0e7e6277386c76ca5
Lucas Barbosa Calixto <81984351+lucasclx@users.noreply.github.com>
2025-05-07 17:46:04 -0300

    commit inicial

---
+++ backend/src/main/java/com/backend/tessera/TesseraApplication.java
@@ -0,0 +1,13 @@
+package com.backend.tessera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesseraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesseraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backend/tessera/TesseraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.tessera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesseraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
